c4c779de26ba8944d1e6dc63f88d0dce6f6e2421
victor-dva <victor.devaradja@gmail.com>
2024-08-18 17:17:34 +0200

    Technical_story_2.0.0 💎
    Create reusable navbar and pagination

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import '../style.css'
+</script>
+
+<div class="navbar">
+    <span>YouFormDoc</span>
+    <a href="/">Accueil</a>
+</div>
+
+<div class="pagination">
+    <a href="#">Accueil</a>
+</div>
+
+<slot />
