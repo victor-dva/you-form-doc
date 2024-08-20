6cba751ba731798c1c96ed1bdd8436a65e977412
victor-dva <victor.devaradja@gmail.com>
2024-08-20 17:10:36 +0200

    Create function displaying Project creation form ✔️

--- src/routes/applicationweb/+page.svelte
+++ src/routes/applicationweb/+page.svelte
@@ -2,17 +2,32 @@
 	import ProjectCard from "$lib/components/ProjectCard.svelte";
     import type { Project } from "$lib/models/applicationWeb";
 
+    /** Store projects */
     let projects: Project[] = [
         {title: 'Facebook', isFinished: false},
         {title: 'Twitter', isFinished: false},
         {title: 'Instagram', isFinished: false}
     ];
     let newProject: Project = {title: '', isFinished: false}
+
+    /** Create a project */
+    let dialog: HTMLDialogElement
+    /** Method called to open project's creation form*/
+    const openDialog = () => {
+        if (dialog) {
+            dialog.showModal()
+        }
+    }
+    /** Method called to open project's creation form*/
+    const closeDialog = () => {
+        dialog.close()
+    }
 </script>
 
 <fieldset>
     <legend>Projets</legend>
-    <button class="create">Créer un projet</button>
+    <button id="createProject" on:click={() => openDialog()}>Créer un projet</button>
+    <!-- Displays created projects -->
     <ul>
         {#each projects as project}
              <ProjectCard title={project.title}/>
@@ -20,7 +35,17 @@
     </ul>
 </fieldset>
 
+<dialog bind:this={dialog}>
+    <h1> Créer un projet </h1>
+    <form>
+        <input placeholder="Nom du projet" required/>
+        <button id="createButton" type="submit">Créer</button>
+    </form>
+    <button id="cancelButton" on:click={() => closeDialog()}>Annuler</button>
+</dialog>
+
 <style>
+    /** Fieldset style */
     fieldset {
         margin-left: 1%;
         margin-right: 1%;
@@ -33,8 +58,21 @@
         color: #fff;
         padding: 3px 6px;
     }
-
-    .create {
+    /** createProject button style*/
+    #createProject {
         float: right;
+    }
+    /** Project creation form*/
+    dialog {
+        text-align: center;
+    }
+    dialog #createButton {
+        background-color: #1a237e;
+        color: white;
+    }
+    dialog #cancelButton {
+        margin-top: 10%;
+        background-color: #1a237e;
+        color: white;
     }
 </style>
