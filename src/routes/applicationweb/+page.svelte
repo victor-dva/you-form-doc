e7196ac2a6593efe06639681cfc5c30e4813d664
victor-dva <victor.devaradja@gmail.com>
2024-08-23 18:02:15 +0200

    User story_5.0.0 ✔️
    Add rename button for each project + Implement update function

--- src/routes/applicationweb/+page.svelte
+++ src/routes/applicationweb/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type { Project } from "$lib/models/applicationWeb";
-	import { addProject, getProjects } from "$lib/services/applicationWeb/projectService";
+	import { addProject, getProjects, updateProject } from "$lib/services/applicationWeb/projectService";
 	import { onMount } from "svelte";
 	import { goto } from '$app/navigation';
 
@@ -13,15 +13,19 @@
     })
 
     /** Dialog */
-    let dialog: HTMLDialogElement
+    let createDialog: HTMLDialogElement
+    let updateDialog: HTMLDialogElement
     /** Method called to open project's creation form*/
-    const openDialog = () => {
+    const openDialog = (dialog: HTMLDialogElement, project: Project | null = null) => {
+        if (project) {
+            currentProject = {id: project.id, title: project.title, isFinished: project.isFinished};
+        }
         if (dialog) {
             dialog.showModal()
         }
     }
     /** Method called to open project's creation form*/
-    const closeDialog = () => {
+    const closeDialog = (dialog: HTMLDialogElement) => {
         dialog.close()
     }
 
@@ -33,9 +37,22 @@
         createButton.disabled = true
         await addProject(newProject);
         projects = await getProjects();
-        closeDialog()
+        closeDialog(createDialog)
         newProject = { title: '', isFinished: false };
         createButton.disabled = false
+    }
+
+    /** Update project */
+    let currentProject: Project = { title: '', isFinished: false }
+    let updateButton: HTMLButtonElement
+
+    const renameProject = async () => {
+        updateButton.disabled = true
+        await updateProject(currentProject.id!, currentProject);
+        projects = await getProjects();
+        closeDialog(updateDialog)
+        currentProject = { title: '', isFinished: false };
+        updateButton.disabled = false
     }
 
     /** Method called to navigate into project page + pass data */
@@ -58,7 +75,7 @@
 
 <fieldset>
     <legend>Projets</legend>
-    <button id="createProject" on:click={() => openDialog()}>Créer un projet</button>
+    <button id="createProject" on:click={() => openDialog(createDialog)}>Créer un projet</button>
     <!-- Display projects -->
     <ul>
         {#each projects as project}
@@ -66,6 +83,10 @@
             <!-- svelte-ignore a11y-click-events-have-key-events -->
             <div class="projectButton" role="button" on:click|preventDefault={() => project && goToProject(project)}>
                 <h2>{project.title}</h2>
+                <button id="updateProject" on:click|preventDefault={event => {
+                    event.stopPropagation();
+                    openDialog(updateDialog, project);
+                }}>📝</button>
             </div>
         {:else}
             <p>Aucun projet</p>
@@ -73,13 +94,22 @@
     </ul>
 </fieldset>
 
-<dialog bind:this={dialog}>
+<dialog bind:this={createDialog}>
     <h1> Créer un projet </h1>
     <form on:submit|preventDefault={createProject}>
         <input bind:value={newProject.title} placeholder="Nom du projet" required/>
         <button bind:this={createButton} id="createButton" type="submit">Créer</button>
     </form>
-    <button id="cancelButton" on:click={() => closeDialog()}>Annuler</button>
+    <button id="cancelButton" on:click={() => closeDialog(createDialog)}>Annuler</button>
+</dialog>
+
+<dialog bind:this={updateDialog}>
+    <h1> Renommer le projet </h1>
+    <form on:submit|preventDefault={renameProject}>
+        <input bind:value={currentProject.title} placeholder="{currentProject.title}" required/>
+        <button bind:this={updateButton} id="updateButton" type="submit">Mettre à jour</button>
+    </form>
+    <button id="cancelButton" on:click={() => closeDialog(updateDialog)}>Annuler</button>
 </dialog>
 
 <style>
@@ -114,6 +144,12 @@
     /** #createProject style*/
     #createProject {
         float: right;
+    }
+
+    /** #updateProject style*/
+    #updateProject {
+        float: right;
+        margin: 0% 5% 0% 0%;
     }
 
     /** dialog style*/
